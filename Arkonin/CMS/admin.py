b258132c3bb8d4ac5c111517bfa98bc769bfdd87
ExploringTheExperiment <shaquilledputra@gmail.com>
2022-04-21 11:24:26 +0700

    Add Link Project for Project and Expert

--- Arkonin/CMS/admin.py
+++ Arkonin/CMS/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Division)
+admin.site.register(Grade)
+admin.site.register(Employee)
+admin.site.register(Expert)
+admin.site.register(SKA)
+admin.site.register(Project)
+admin.site.register(LinkProject)
--- Arkonin/CMS/models.py
+++ Arkonin/CMS/models.py
@@ -5,13 +5,20 @@ class Division(models.Model):
     div_name = models.CharField(max_length=150, null = True)
     div_code = models.CharField(max_length=3, null = True)
 
+    def __str__(self):
+        return self.div_name
+
 class Grade(models.Model):
     grade_name = models.CharField(max_length=150, null=True)
     grade_code = models.CharField(max_length=3, null = True)
 
+    def __str__(self):
+        return self.grade_name
+
+
 class Employee(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    nip = models.IntegerField(max_length=11, null = True)
+    nip = models.CharField(max_length=11, null = True)
     name = models.CharField(max_length=150, null = True)
     division = models.ForeignKey(Division, null = True, on_delete = models.SET_NULL)
     grade = models.ForeignKey(Grade, null = True, on_delete = models.SET_NULL)
@@ -20,11 +27,17 @@ class Employee(models.Model):
     male = models.BooleanField(default=False)
     female = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.nip
+
 class Expert(models.Model):
     exp_name = models.ForeignKey(Employee, null = True, on_delete= models.SET_NULL)
     exp_title = models.CharField(max_length=200, null = True)
     exp_education = models.CharField(max_length=200, null = True)
     exp_graduate = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.exp_name
 
 class SKA(models.Model):
     exp_name = models.ForeignKey(Expert, null=True, on_delete= models.SET_NULL)
@@ -33,6 +46,10 @@ class SKA(models.Model):
     publish_date = models.DateField(auto_now=False, auto_now_add=False)
     expire_date = models.DateField(auto_now=False, auto_now_add=False)
 
+    def __str__(self):
+        return self.exp_name
+
+
 class Project(models.Model):
     division = models.ForeignKey(Division, null=True, on_delete= models.SET_NULL)
     project_name = models.CharField(max_length=200, null=True)
@@ -40,5 +57,14 @@ class Project(models.Model):
     start_date = models.DateField(auto_now=False, auto_now_add=False)
     end_date = models.DateField(auto_now=False, auto_now_add=False)
     bast_date = models.DateField(auto_now=False, auto_now_add=False)
-    bast_value = models.IntegerField(max_length=3, null = True)
+    bast_value = models.CharField(max_length=3, null = True)
+
+    def __str__(self):
+        return self.project_name
+
+class LinkProject(models.Model):
+    project = models.ForeignKey(Project, null=True, on_delete= models.CASCADE)
+    expert = models.ForeignKey(Expert, null=True, on_delete= models.CASCADE)
+
+
 
