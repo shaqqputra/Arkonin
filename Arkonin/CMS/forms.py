1a9ce8060b32a7e6b5afc6700b4f4126d673c518
ExploringTheExperiment <shaquilledputra@gmail.com>
2022-05-25 12:22:43 +0700

    register validation

--- Arkonin/CMS/forms.py
+++ Arkonin/CMS/forms.py
@@ -5,6 +5,28 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['division','project_name', 'project_file', 'start_date', 'end_date', 'bast_date', 'bast_value']
+        labels = {
+            'division': ('Divisi'),
+            'project_name': ('Nama Projek'),
+            'project_file': ('File Projek'),
+            'start_date': ('Tanggal Mulai'),
+            'end_date': ('Tanggal Selesai'),
+            'bast_date': ('Tanggal BAST'),
+            'bast_value': ('Nilai BAST')
+        }
+
+        error_messages = {
+            'project_name': {
+                'max_length': ("Nama Terlalu Panjang"),
+            }
+        }
+
+        def clean(self):
+            cleaned_data = super().clean()
+            start_date = cleaned_data.get("start_date")
+            end_date = cleaned_data.get("end_date")
+            if end_date < start_date:
+                raise forms.ValidationError("End date should be greater than start date.")
 
         widgets = {
             'division' : forms.Select(attrs={'class':'form-control', 'id':'inputGroupSelect01'}),
