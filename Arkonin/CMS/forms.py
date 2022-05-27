95016c5d9d7fb57a02b249c960824497f260247c
ExploringTheExperiment <shaquilledputra@gmail.com>
2022-05-27 14:32:24 +0700

    validation

--- Arkonin/CMS/forms.py
+++ Arkonin/CMS/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from .models import Project
 from django.core.files.images import get_image_dimensions
+
 class PostForm(forms.ModelForm):
+    
     class Meta:
         model = Project
-        fields = ['division','project_name', 'project_file', 'start_date', 'end_date', 'bast_date', 'bast_value']
+        fields = ['division',
+        'project_name', 
+        'project_file', 
+        'start_date', 
+        'end_date', 
+        'bast_date', 
+        'bast_value']
         labels = {
             'division': ('Divisi'),
             'project_name': ('Nama Projek'),
@@ -18,8 +26,18 @@ class PostForm(forms.ModelForm):
         error_messages = {
             'project_name': {
                 'max_length': ("Nama Terlalu Panjang"),
-            }
+            },
+
+            'bast_value': {
+                'max_length': ("Maksimal Adalah 3 Huruf!")
+            },
         }
+
+        def clean_project_name(self):
+            project_name = self.cleaned_data.get('project_name')
+            if not project_name:
+                raise forms.ValidationError('This field is required')
+            return project_name
 # Validasi untuk Range Date, Tanggal Selesai Harusnya Lebih Besar Daripada Tanggal Mulai *Masih Error
         def clean(self):
             cleaned_data = super().clean()
@@ -37,5 +55,6 @@ class PostForm(forms.ModelForm):
             'start_date' : forms.DateInput(format=('%d-%m-%Y'), attrs={'class':'form-control','type':'date','placeholder':'Select Your Date', 'id' : 'startdate'}),
             'end_date' : forms.DateInput(format=('%d-%m-%Y'), attrs={'class':'form-control','type':'date','placeholder':'Select Your Date', 'id' : 'enddate'}),
             'bast_date' : forms.DateInput(format=('%d-%m-%Y'), attrs={'class':'form-control','type':'date','placeholder':'Select Your Date'}),
-            'bast_value' : forms.NumberInput(attrs={'class':'form-control'}),
+            'bast_value' : forms.NumberInput(attrs={'class':'form-control',}),
         }
+
